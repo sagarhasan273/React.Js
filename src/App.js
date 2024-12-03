342aecedf9a01f2bb11861f7654d82e4b06fb82f
sagarhasan273 <sagarhasan273@gmail.com>
2024-12-03 10:40:08 +0600

    viewport height and width

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './app.css';
-import Units from './component/units';
 
 export default function App() {
   return (
-    <div>
-      <Units />
+    <div className="full-height">
+      <div className="half-width">This is 50% of the viewport's width.</div>
     </div>
   );
 }
